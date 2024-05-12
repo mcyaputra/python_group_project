7a9a0c2431608fdcab07ddc46ad3d6264805bf03
Michael <michael.yaputra@gmail.com>
2024-05-12 15:34:13 +1000

    Adding OpenAI to summarize news content

--- core/openai_api.py
+++ core/openai_api.py
@@ -1,10 +1,19 @@
+import logging
 
+# Set up logs
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(funcName)s:%(message)s")
 
+# Save logs in a log file
+file_handler = logging.FileHandler("logs.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 class OpenAISummariser:
     def __init__(self, openai_client):
         self.openai_client = openai_client
-    
+     
     def execute_prompt(self, 
                        prompt, 
                        system_prompt="You are an article summariser", 
@@ -28,4 +37,15 @@ class OpenAISummariser:
         }
         return result
     
-    def 
+    def summarise_news_content(self, 
+                               content):
+        prompt = f"""
+                You are a great article summarizer. 
+                Please summarize the following text into 200-250 words, highlighting the 3-5 most important takeaways for the readers:{content}
+                """
+            
+        result = self.execute_prompt(prompt=prompt)
+		
+        logger.info("Successfully summarize news content")
+        return result["value"]
+        
--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import os
 from core.worldnews_api import NewsAPI
+from openai import OpenAI
 from core.utils import unpack_response
-import pandas as pd
+from core.openai_api import OpenAISummariser
 
-api_key = os.environ.get('WORLDNEWS_API_KEY')
+
+news_api_key = os.environ.get('WORLDNEWS_API_KEY')
+
+OPENAI_CLIENT = OpenAI(
+	api_key=os.environ.get("OPENAI_API_KEY")
+)
+open_AI = OpenAISummariser(openai_client=OPENAI_CLIENT)
 
 '''
     To get free API KEY, go to: https://worldnewsapi.com/
 '''
 
-news_api = NewsAPI(api_key)
+news_api = NewsAPI(news_api_key)
 
 # TODO Replace below with actual inputs
 keyword1 = "Cars" # Mandatory
@@ -20,8 +27,8 @@ country2 = "" # Optional
 country3 = "" # Optional
 min_sentiment = -1 # Optional, default is -1
 max_sentiment = 1 # Optional, default is 1
-from_date = '2024-05-10' # Optional, need to set default -7 days from today in GUI
-to_date =  '2024-05-11' # Optional, need to set default to today in GUI
+from_date = '2024-05-11' # Optional, need to set default -1 day from today in GUI
+to_date =  '2024-05-12' # Optional, need to set default to today in GUI
 article_to_show = 5 # Optional, default is 5
 
 # Execute search
@@ -41,3 +48,9 @@ search_result = news_api.search_news(
 
 # Unpack search result
 title, author, publish_date, content, url, sentiment, source_country = unpack_response(search_result)
+
+# Method to summarize news content in 250 words, highlighting 3-5 most important takeaways in bullet points
+# We can let users choose whether they want to get a quick summary or not based on title etc
+summary_result = open_AI.summarise_news_content(content[0]) ## TODO replace content with actual news content 
+
+print(summary_result)
